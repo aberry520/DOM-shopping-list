5e3729b54f570d5e1703e60b27818c9bd1d669e6
Austin Berry <aberry52098@gmail.com>
2023-09-01 15:24:43 -0400

    put text input into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,20 @@
 'use strict';
+const myList = document.querySelector("#myList");
+const generateList = document.querySelector("#generateList");
+
+generateList.addEventListener("click", arr);
+function arr(event){
+    event.preventDefault();
+    const one = document.getElementById("one").value;
+    const two = document.getElementById("two").value;
+    const three = document.getElementById("three").value;
+    const four = document.getElementById("four").value;
+    const input = [one,two,three,four];
+    console.log(input);
+};
+
+
+
+//need to get the text input; put it in an array maybe?
+//js needs to create an ls when button is pressed
+//need to insert that text input into a list
