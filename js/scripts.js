6bc046dfe131599521a0b5c46083b9c0f5ea8484
Austin Berry <aberry52098@gmail.com>
2023-09-03 19:45:47 -0400

    add clear function, and fully redid array

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,55 @@
 'use strict';
+/////global variables\\\\\
 const myList = document.querySelector("#myList");
-const generateList = document.querySelector("#generateList");
+const generateList = document.querySelector("#generateList"); 
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+let array = [];
+//////////////////////////
 
+//pushes items into an array
 function arr(){
-    const one ="<ul>"+"<li>" + document.getElementById("one").value + "</li>";
-    const two = "<li>" + document.getElementById("two").value + "</li>";
-    const three = "<li>" + document.getElementById("three").value + "</li>";
-    const four = "<li>" + document.getElementById("four").value + "</li>"+"</ul>";
-    return one + two + three + four;
+    const listBegin="<ul>";
+    const listEnd="</ul>";
+    if (one.value !== "") {
+        array.push("<li>" + one.value + "</li>");
+      }
+    
+      if (two.value !== "") {
+        array.push("<li>" + two.value + "</li>");
+      }
+    
+      if (three.value !== "") {
+        array.push("<li>" + three.value + "</li>");
+      }
+    
+      if (four.value !== "") {
+        array.push("<li>" + four.value + "</li>");
+      }
+    
+    return listBegin + array.join("") + listEnd;
 };
+//sets each value to ""
+function reset(){
+    one.value = "";
+    two.value = "";
+    three.value = "";
+    four.value = "";
+}
 
+//clears list
+clearList.addEventListener("click", clear);
+function clear(){
+    myList.innerHTML = "";//removes html
+    array = [];//resets array to empty
+};
+//generates list
 generateList.addEventListener("click", list);
 function list(event){
     event.preventDefault();
-    myList.innerHTML = arr();
+    if (myList.innerHTML = arr()){
+        reset();
+    };
 };
-
-//need to get the text input; put it in an array maybe?
-//js needs to create an ls when button is pressed
-//need to insert that text input into a list
